a7d9a906e29c3f29dc8d3932a9d079d10be7cdb3
soviaawahyuningtyas <137589189+soviaawahyuningtyas@users.noreply.github.com>
2023-07-01 20:35:07 +0700

    modified file coding

--- script.js
+++ script.js
@@ -28,3 +28,55 @@
             document.body.classList.remove('active');
         }
     }
+
+function toPage(page){
+    let elmntToView = document.getElementById(page);
+    elmntToView.scrollIntoView(); 
+}
+
+function handleGetFormData(){
+    let name = document.getElementById("name").value
+    let email = document.getElementById("email").value
+    let city = document.getElementById("city").value
+    let zipCode = document.getElementById("zip-code").value
+    let status = document.getElementById("status").checked
+    return {
+        name: name,
+        email : email, 
+        city : city, 
+        zipCode : zipCode,
+        status : status
+    }
+}
+
+function isNumber(string){
+    return !isNaN(string);
+}
+
+function checkboxIsChecked(){
+     let status = document.getElementById("status").checked
+     return status;
+}
+
+function validateFormData(objek){
+    if(objek != null && isNumber(objek.zipCode) && checkboxIsChecked()){
+        return true
+    }
+    return false
+}
+
+function submit(){
+    let warning = document.getElementById("warning")
+    if(validateFormData(handleGetFormData()) == false){
+        warning.innerHTML = "Periksa form anda sekali lagi"
+    }else{
+         warning.innerHTML = ""
+    }
+    
+}
+
+
+document.getElementById("submit-form").addEventListener('click', function(event){
+  event.preventDefault()
+  submit()
+});
